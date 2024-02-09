c0626be1a3f79fa86038e826ad47670864df35a6
Hardik Agarwal <hardikagarwal9308@gmail.com>
2024-02-09 19:57:38 +0000

    feat(frontend): setup main application entry point and providers

---
+++ backend/src/modules/invoices/invoice.controller.ts
@@ -0,0 +1,50 @@
+import { sendSuccess } from "../../utils/apiResponse.js";
+import { Request, Response } from "express";
+
+import { invoiceService } from "./invoice.service.js";
+
+export const invoiceController = {
+  createInvoice: async (req: Request, res: Response) => {
+    const invoice = await invoiceService.createInvoice(
+      req.organization!.id,
+      req.user!.id,
+      req.body,
+    );
+    sendSuccess(res, { statusCode: 201, data: invoice });
+  },
+  updateInvoice: async (req: Request, res: Response) => {
+    const invoice = await invoiceService.updateInvoice(
+      req.organization!.id,
+      req.user!.id,
+      req.params.id as string,
+      req.body,
+    );
+    sendSuccess(res, { statusCode: 200, data: invoice });
+  },
+  listInvoices: async (req: Request, res: Response) => {
+    const invoices = await invoiceService.listInvoices(
+      req.organization!.id,
+      {
+        status: req.query.status as string | undefined,
+        search: req.query.search as string | undefined,
+      },
+      req.query,
+    );
+    sendSuccess(res, { statusCode: 200, data: invoices });
+  },
+  getInvoice: async (req: Request, res: Response) => {
+    const invoice = await invoiceService.getInvoice(
+      req.organization!.id,
+      req.params.id as string,
+    );
+    sendSuccess(res, { statusCode: 200, data: invoice });
+  },
+  deleteInvoice: async (req: Request, res: Response) => {
+    await invoiceService.deleteInvoice(
+      req.organization!.id,
+      req.user!.id,
+      req.params.id as string,
+    );
+    sendSuccess(res, { statusCode: 200, message: "Invoice deleted successfully" });
+  },
+};
