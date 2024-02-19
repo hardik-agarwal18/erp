3df6dab62c2d008a4db800e0b0aaf19453c361f8
Hardik Agarwal <hardikagarwal9308@gmail.com>
2024-02-19 19:57:38 +0000

    feat(frontend): configure routing system

---
+++ backend/src/modules/organizations/index.ts
@@ -0,0 +1,6 @@
+export { organizationController } from "./organization.controller.js";
+export { organizationService } from "./organization.service.js";
+export { organizationRepository } from "./organization.repository.js";
+export * from "./organization.types.js";
+export { organizationContextMiddleware } from "./organization.middleware.js";
+export { default as organizationRoutes } from "./organization.routes.js";
---
+++ backend/src/modules/organizations/organization.middleware.ts
@@ -0,0 +1,5 @@
+import { tenantContextMiddleware } from "../../middleware/tenant.middleware.js";
+
+export const organizationContextMiddleware = tenantContextMiddleware({
+  allowRouteParam: true,
+});
