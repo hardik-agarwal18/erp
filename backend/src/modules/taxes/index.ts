f56971fa6b49697f9efb5883d0181ba065c39f67
Hardik Agarwal <hardikagarwal9308@gmail.com>
2024-04-09 19:57:38 +0000

    feat(frontend): add layout components (sidebar, header)

---
+++ backend/src/modules/taxes/index.ts
@@ -0,0 +1,5 @@
+export { taxController } from "./tax.controller.js";
+export { taxService } from "./tax.service.js";
+export { taxRepository } from "./tax.repository.js";
+export * from "./tax.types.js";
+export { default as taxRoutes } from "./tax.routes.js";
